04ca02c483f0aa39b0d18fb498f35ae691b0524c
BradenCradock <bgcradock@gmail.com>
2018-10-09 11:41:54 +1300

    Tidied CompilationEngine by adding a few new methods

--- projects/10/Compiler/CompilationEngine.py
+++ projects/10/Compiler/CompilationEngine.py
@@ -12,134 +12,92 @@ class CompilationEngine:
 
         self.compileClass()
 
+    def writeXml(self):
+        rep = {"<"  : "&lt;",
+               ">"  : "$gt;",
+               "\"" : "&quot;",
+               "&"  : "&amp;"}
+        rep = dict((re.escape(k), v) for k, v in rep.items())
+        pattern = re.compile("|".join(rep.keys()))
+        token = pattern.sub(lambda m: rep[re.escape(m.group(0))], self.tokenizer.currentToken)
+
+        print("\t" * self.xmlIndentation + "<" + self.tokenizer.currentTokenType + "> " + token + " </" + self.tokenizer.currentTokenType + "> ", file = self.file)
+
+
+    def syntaxError(self, expected, recieved):
+        print("</class>", file = self.file)
+        sys.exit("Invalid Syntax: Expected " + expected + "but recieved " + recieved)
+
+    def checkTokenMatch(self, string):
+        if self.tokenizer.currentToken in string:
+            self.writeXml()
+            self.tokenizer.advance()
+        else:
+            self.syntaxError(string, self.tokenizer.currentToken)
+
+    def checkToken(self, string):
+        if self.tokenizer.currentTokenType in string:
+            self.writeXml()
+            self.tokenizer.advance()
+        else:
+            self.syntaxError(string, self.tokenizer.currentTokenType)
+
+    def checkVarType(self):
+        if self.tokenizer.currentToken in self.types:
+            self.writeXml()
+            self.tokenizer.advance()
+        else:
+            print("</class>", file = self.file)
+            sys.exit("Invalid Syntax: " + self.tokenizer.currentToken + "is not a valid type.")
+
     #Compiles a complete class.
     def compileClass(self):
         self.tokenizer.advance()
         print("<class>", file = self.file)
         self.xmlIndentation += 1
 
-        if self.tokenizer.currentToken == "class":
-            self.writeXml()
-            self.tokenizer.advance()
+        self.checkToken("class")
+        self.checkTokenType("IDENTIFIER")
+        self.checkToken("{")
 
-            if self.tokenizer.currentTokenType == "IDENTIFIER":
-                self.writeXml()
-                self.tokenizer.advance()
+        while self.tokenizer.currentToken not in {"}", "constructor", "function", "method", "void"} :
+            self.compileClassVarDec()
+        while self.tokenizer.currentToken != "}":
+            self.compileSubroutine()
 
-                if self.tokenizer.currentToken == "{":
-                    self.writeXml()
-                    self.tokenizer.advance()
-
-                    while self.tokenizer.currentToken not in {"}", "constructor", "function", "method", "void"} :
-                        if self.tokenizer.currentToken in {"field", "static"}:
-                            self.compileClassVarDec()
-                        else:
-                            print("</class>", file = self.file)
-                            sys.exit("Invalid Syntax: Expected declaration of a variable or subrotuine.")
-
-                    while self.tokenizer.currentToken != "}":
-                        if self.tokenizer.currentToken in {"constructor", "function", "method", "void"}:
-                            self.compileSubroutine()
-                        elif self.tokenizer.currentToken in {"field", "static"}:
-                            print("</class>", file = self.file)
-                            sys.exit("Invalid Syntax: Declaration of all class variables must occur before subrotuines.")
-                        else:
-                            print("</class>", file = self.file)
-                            sys.exit("Invalid Syntax: Expected declaration of a variable or subrotuine.")
-
-                else:
-                    print("</class>", file = self.file)
-                    sys.exit("Invalid Syntax: Expected \"{\" after class identifier.")
-            else:
-                print("</class>", file = self.file)
-                sys.exit("Invalid Syntax: Expected identifier after class declaration.")
-        else:
-            print("</class>", file = self.file)
-            sys.exit("Invalid Syntax: Module must begin with class declaration.")
         print("</class>", file = self.file)
         self.xmlIndentation -= 1
-        return
+
 
     #Compiles a static or field declaration.
     def compileClassVarDec(self):
         print("\t" * self.xmlIndentation + "<classVarDec>", file = self.file)
         self.xmlIndentation += 1
-        self.writeXml()
-        self.tokenizer.advance()
 
-        if self.tokenizer.currentToken in self.types:
-            self.writeXml()
-            self.tokenizer.advance()
-
-            if self.tokenizer.currentTokenType == "IDENTIFIER":
-                self.writeXml()
-                self.tokenizer.advance()
-
-                while self.tokenizer.currentToken != ";":
-                    if self.tokenizer.currentToken == ",":
-                        self.writeXml()
-                        self.tokenizer.advance()
-                        if self.tokenizer.currentTokenType == "IDENTIFIER":
-                            self.writeXml()
-                            self.tokenizer.advance()
-
-                        else:
-                            print("</class>", file = self.file)
-                            sys.exit("Invalid Syntax: Variable names cannot begin with a digit.")
-                    else:
-                        print("</class>", file = self.file)
-                        sys.exit("Invalid Syntax: Expected \",\" or \";\"")
-
-                self.writeXml()
-                self.tokenizer.advance()
-
-            else:
-                print("</class>", file = self.file)
-                sys.exit("Invalid Syntax: Idenfifier cannot begin with a digit.")
-        else:
-            print("</class>", file = self.file)
-            sys.exit("Invalid syntax or invalid variable type.")
+        self.checkToken({"field", "static"})
+        self.checkVarType()
+        self.checkTokenType("IDENTIFIER")
+        while self.tokenizer.currentToken != ";":
+            self.checkToken(",")
+            self.checkTokenType("IDENTIFIER")
+        self.checkToken(";")
 
         self.xmlIndentation -= 1
         print("\t" * self.xmlIndentation + "</classVarDec>", file = self.file)
-        return
 
     #Compiles a complete method, function or constructor.
     def compileSubroutine(self):
         print("\t" * self.xmlIndentation + "<subroutineDec>", file = self.file)
         self.xmlIndentation += 1
-        self.writeXml()
-        self.tokenizer.advance()
-        if (self.tokenizer.currentToken in self.types) | (self.tokenizer.currentToken == "void"):
-            self.writeXml()
-            self.tokenizer.advance()
 
-            if self.tokenizer.currentTokenType == "IDENTIFIER":
-                self.writeXml()
-                self.tokenizer.advance()
+        self.checkToken({"constructor", "function", "method", "void"})
+        self.checkToken(self.types.append("void"))
+        self.checkTokenType("IDENTIFIER")
+        self.checkToken("(")
+        self.compileParameterList()
+        self.checkToken(")")
+        self.compileSubroutineBody()
 
-                if self.tokenizer.currentToken == "(":
-                    self.writeXml()
-                    self.tokenizer.advance()
-                    self.compileParameterList()
-
-                    if self.tokenizer.currentToken == ")":
-                        self.writeXml()
-                        self.tokenizer.advance()
-                        self.compileSubroutineBody()
-
-                    else:
-                        print("</class>", file = self.file)
-                        sys.exit("Invalid Syntax: Expected \")\" after definition of function parameters.")
-                else:
-                    print("</class>", file = self.file)
-                    sys.exit("Invalid Syntax: Expected \"(\" after subroutine name.")
-            else:
-                print("</class>", file = self.file)
-                sys.exit("Invalid Syntax: Subroutine names cannot begin with a digit.")
-        else:
-            print("</class>", file = self.file)
-            sys.exit("Invalid subrotuine type.")
         self.xmlIndentation -= 1
         print("\t" * self.xmlIndentation + "</subroutineDec>", file = self.file)
         return
@@ -149,12 +107,11 @@ class CompilationEngine:
     def compileSubroutineBody(self):
         print("\t" * self.xmlIndentation + "<subroutineBody>", file = self.file)
         self.xmlIndentation += 1
-        if self.tokenizer.currentToken == "{":
-            self.writeXml()
-            self.tokenizer.advance()
-            while self.tokenizer.currentToken != "return":
-                if self.tokenizer.currentToken = "var":
 
+        self.checkToken("{")
+        while self.tokenizer.currentToken != "return":
+            if self.tokenizer.currentToken == "var":
+                    self.compileVarDec()
             if self.tokenizer.currentToken == "}":
                 self.writeXml()
                 self.tokenizer.advance()
@@ -165,14 +122,13 @@ class CompilationEngine:
         else:
             print("</class>", file = self.file)
             sys.exit("Invalid Syntax: Expected \"{\" after declaration of function.")
-        while
         return
     #Compiles a (possibly empty) parameter list, not including the enclosing"()".
     def compileParameterList(self):
         print("\t" * self.xmlIndentation + "<parameterList>", file = self.file)
         self.xmlIndentation += 1
 
-        while self.tokenizer.currentToken != ")":
+        #while self.tokenizer.currentToken != ")":
 
         return
 
@@ -219,14 +175,3 @@ class CompilationEngine:
     #Compiles a (possibily empty) comma-seperated list of expressions.
     def compileExpressionList(self):
         return
-
-    def writeXml(self):
-        rep = {"<"  : "&lt;",
-               ">"  : "$gt;",
-               "\"" : "&quot;",
-               "&"  : "&amp;"}
-        rep = dict((re.escape(k), v) for k, v in rep.items())
-        pattern = re.compile("|".join(rep.keys()))
-        token = pattern.sub(lambda m: rep[re.escape(m.group(0))], self.tokenizer.currentToken)
-
-        print("\t" * self.xmlIndentation + "<" + self.tokenizer.currentTokenType + "> " + token + " </" + self.tokenizer.currentTokenType + "> ", file = self.file)
